45695f4fc0069fd87915c8254f9e37d394c19840
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2019-06-30 17:18:07 -0300

    Aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do carro é ${vel}km/h`)
+if (vel >= 60) { // Condicao simples
+    console.log('Voce ultrapssou a velocidade permitida. MULTADO!')
+}
+console.log('Error')
